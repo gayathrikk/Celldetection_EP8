7673d036857f73f289bfb6aee39ba0068af24152
gayathrikk <gayathri@htic.iitm.ac.in>
2024-01-03 17:31:44 +0530

    update commit

--- src/test/java/Sanity/Cell_annotation/Testing.java
+++ src/test/java/Sanity/Cell_annotation/Testing.java
@@ -303,8 +303,8 @@ public class Testing {
 			  driver.switchTo().defaultContent();
 			  clickElementByXpath("//a[@title='Actions']", "Action"); 
 			  clickElementByXpath("//a[@title='Save']", "save"); 
-			  Thread.sleep(2000);
-			 WebElement toast4 = driver.findElement(By.xpath("//div[text()='Saved Successfullly']"));
+			  Thread.sleep(4000);
+		   	 WebElement toast4 = driver.findElement(By.xpath("//div[text()='Saved Successfullly']"));
 			  String actualFileName4 = toast4.getText();
 			  String expectedFileName4 = "Saved Successfullly"; 
 			  Assert.assertEquals(actualFileName4, expectedFileName4);
